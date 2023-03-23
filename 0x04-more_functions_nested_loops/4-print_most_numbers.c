7927cc95a81609438b1f71aa57bbece75693984a
othmane-Z <othmanezine7@gmail.com>
2023-03-23 10:02:31 -0700

    alx

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_most_numbers - Print numbers between 0 to 9 incl
+ * Return: Void
+ */
+void print_most_numbers(void)
+{
+	int i;
+
+	for (i = 48; i < 58; i++)
+	{
+		if (i == 50 || i == 52)
+			continue;
+		_putchar(i);
+	}
+	_putchar('\n');
+}
