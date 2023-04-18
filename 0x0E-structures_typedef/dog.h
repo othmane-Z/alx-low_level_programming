e6b7d0eaa197b023d1d4b1c4943cb70854e8bcdf
othmane-Z <othmanezine7@gmail.com>
2023-04-17 18:12:10 -0700

    alx

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,24 @@
+#ifndef DOG_H
+#define DOG_H
+/**
+ * struct dog - dog's profile
+ * @name: dog's name
+ * @age: dog's age
+ * @owner: dog's owner
+ * Description: dog's profile
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+typedef struct dog dog_t;
+
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+void free_dog(dog_t *d);
+dog_t *new_dog(char *name, float age, char *owner);
+
+#endif
